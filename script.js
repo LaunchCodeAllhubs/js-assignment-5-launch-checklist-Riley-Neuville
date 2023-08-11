d975ae4c6e074f166152be7d34f5f5742eaaf134
Riley Neuville <Riley.Neuville@gmail.com>
2023-08-10 23:54:39 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+
+const form = document.getElementById("testForm");
+const list = document.getElementsByTagName("ol");
+const pilot = document.getElementById("pilotName");
+const copilot = document.getElementById("copilotName");
+const fuelLevel = document.getElementById("fuelLevel");
+const cargoMass = document.getElementById("cargoMass");
+/*window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
@@ -13,4 +20,8 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
+});*/
+form.addEventListener("submit", function(event){
+event.preventDefault();
+formSubmission(document,list, pilot.value, copilot.value, fuelLevel.value, cargoMass.value);
 });
